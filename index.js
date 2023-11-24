7749f13e686a431cffab66fdf22987b64247cbf9
NamLuu199 <58756031+NamLuu199@users.noreply.github.com>
2023-11-25 00:07:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 setTimeout(() => {
     document.querySelector('#comp-lokan5jq').addEventListener('click', () => {
         FB.CustomerChat.showDialog()
+        console.log('hihi')
     })
 }, 1000)
